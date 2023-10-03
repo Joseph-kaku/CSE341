0827a2cc3f7d653c86ea5e8d924618e2e24b5a27
Joseph-kaku <samkaks18@gmail.com>
2023-10-02 18:14:16 -0600

    L04 completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,4 +1,10 @@
 const routes = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 
 const controllers = require('../controllers')
 
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description',
+  },
+  host: 'cse341-a2oa.onrender.com',
+  schemes: ['https'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/contacts.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
