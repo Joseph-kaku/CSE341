df1874e06f2af951cfdcc039c8a1f98d9d6e411a
Joseph-kaku <samkaks18@gmail.com>
2023-10-04 15:27:14 -0600

    had to fix swagger

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: ( 'localhost:8080'),
-  schemes: ['http'],
+  host: ['localhost:8080'],
+  schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger-output.json';
