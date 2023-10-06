f93d7ea10c5f4f24e4cd815bfb5bb794f34d8e2b
Joseph-kaku <samkaks18@gmail.com>
2023-10-06 16:50:11 -0600

    L04 last commit

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: ['localhost:8080'],
-  schemes: ['http', 'https'],
+  host: ['cse341-a2oa.onrender.com'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
